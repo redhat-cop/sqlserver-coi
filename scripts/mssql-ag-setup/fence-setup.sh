fe4a4c78da2a2d53e9944a988f399a8ccba7be08
Louis Imershein <33468073+limershein@users.noreply.github.com>
2020-09-14 16:59:39 -0700

    Add files via upload

---
+++ scripts/mssql-ag-setup/fence-setup.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# fence-setup.sh - any fencing resource agent settings should go here.
+#                  with baremetal you can enable a watchdog and skip the complexity
+
+# Bring in the configuration parameters
+source ./params.sh
+
+echo "Turn on fencing. A watchdog timer is all you need for barmetal/KVM"
+# Enable fencing, on baremetal or KVM we just use a watchdog timer
+ssh root@$PRIMARY_SERVER  pcs property set stonith-watchdog-timeout=10s
+ssh root@$PRIMARY_SERVER pcs stonith sbd enable
+
